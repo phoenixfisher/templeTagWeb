f5ff6d6bb95ba644b9647dba0b1e0e0526bcbfb6
Phoenix Fisher <phoenixfisher702@gmail.com>
2025-12-08 09:13:51 -0700

    start with landing page

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views")); 
 
 app.get("/", (req, res) => {
-  res.render("login");
+  res.render("landing");
 });
 
 app.listen(PORT, () => {
