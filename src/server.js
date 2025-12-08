42303f15eb6a6786dcf5df6bf2e81419ddac006c
Phoenix Fisher <phoenixfisher702@gmail.com>
2025-12-08 08:56:29 -0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,20 @@
+// app/index.js
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+// Tell Express to use EJS
+app.set("view engine", "ejs");
+
+// Point to the correct views directory
+app.set("views", path.join(__dirname, "views")); 
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
